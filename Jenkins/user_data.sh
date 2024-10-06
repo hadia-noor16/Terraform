bb30b26695bb9ab058c198a955dfc827c6d5db3a
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2024-10-06 12:13:18 -0600

    Creating EC2 instance with Jenkins installed

--- Jenkins/user_data.sh
+++ Jenkins/user_data.sh
@@ -6,6 +6,6 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo dnf install java-17-amazon-corretto -y
 sudo yum install jenkins -y
-sudo systemctl enable Jenkins
-sudo systemctl start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 sudo systemctl status jenkins
