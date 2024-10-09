20d04893168da5852e2ffd5a986bc5e61e0306b0
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2024-10-08 18:09:24 -0600

    Adding Variables.tf and provider.tf to jenkins project

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,11 +1,8 @@
-provider "aws" {
-region = "us-east-1"
-}
 # Creating securoty group to open SSH port from your IP, port 8080 to open for all traffic and allowing all outbound traffic
 resource "aws_security_group" "tf_jenkins_sg" {
   name        = "tf_jenkins_sg"
   description = "Allow https inbound traffic and SSH from my IP"
-  vpc_id      = "vpc-0af945834a669fd8d"
+  vpc_id      = var.vpc_id
   ingress {
     from_port   = 22
     to_port     = 22
@@ -33,11 +30,11 @@ resource "aws_security_group" "tf_jenkins_sg" {
 
 # Creating an EC2 instance with Amazon Linux AMI ID, SG, key pair to associate and user data to install in EC2
 resource "aws_instance" "jenkins-ec2" {
-  ami           = "ami-0fff1b9a61dec8a5f"
-  instance_type = "t3.micro"
-  vpc_security_group_ids =[aws_security_group.tf_jenkins_sg.id]
-  key_name  = aws_key_pair.key_pair.key_name
-  user_data = file("user_data.sh")
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.tf_jenkins_sg.id]
+  key_name               = aws_key_pair.key_pair.key_name
+  user_data              = file("user_data.sh")
   tags = {
     Name = "jenkins-ec2"
   }
