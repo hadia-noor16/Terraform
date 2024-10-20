08a3b969312603effb7222ea80c120607ea915b8
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2024-10-19 21:10:19 -0600

    adding ALB id in outputs.tf

--- two-tier/outputs.tf
+++ two-tier/outputs.tf
@@ -2,7 +2,10 @@ output "alb_dns_name" {
   value = aws_lb.myalb.dns_name
 }
 
-
 output "rds_endpoint" {
     value = aws_db_instance.two-tier-rds.endpoint
+} 
+
+output "alb_id" {
+    value = aws_lb.myalb.id
 }
