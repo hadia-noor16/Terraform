93789a5cfe6ef05d370edb55845d241f38026c63
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2024-10-28 19:24:38 -0600

    Update main.tf

--- two-tier/main.tf
+++ two-tier/main.tf
@@ -274,12 +274,12 @@ resource "aws_lb_target_group" "my_tg" {
   target_type = "instance"
 
   health_check {
-    interval            = 30
+    interval            = 20
     path                = "/"
     port                = 80
     healthy_threshold   = 5
     unhealthy_threshold = 2
-    timeout             = 5
+    timeout             = 3
     protocol            = "HTTP"
     matcher             = "200,202"
   }
